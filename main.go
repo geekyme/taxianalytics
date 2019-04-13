fdf372bffa2b8e77795a1850b09743cb63456992
Shawn Lim <shawnlim89@live.com>
2019-04-13 22:49:37 +0800

    Add http handler for /

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+const defaultPort = "8080"
 
 func main() {
-	fmt.Println("Hello world")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, world")
+	})
+
+	log.Printf("Server running at port %s", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
