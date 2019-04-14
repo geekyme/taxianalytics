6bf26ed2261bf2885930005122654c452eb6c522
Shawn Lim <shawnlim89@live.com>
2019-04-14 17:46:44 +0800

    Fix deployment into minikube

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM golang:1.11.2
+FROM golang:1.12.3
 
 WORKDIR /taxianalytics
 
--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,31 @@ echo "#### Running script to deploy to Minikube..."
 
 APP=taxianalytics
 VERSION=$(git rev-parse --short HEAD)
+GCLOUD_KEY_FILE=key.json
+
+if [ -z "$TAXI_TOPIC" ]
+then
+  echo "env TAXI_TOPIC must be set!"
+  exit 1
+fi
+
+if [ -z "$TAXI_PROJECT" ]
+then
+  echo "env TAXI_PROJECT must be set!"
+  exit 1
+fi
 
 echo "#### Current version: $VERSION"
-
 export DOCKER_IMAGE_TAG=local/$APP:$VERSION
+
+echo "#### Finding gcloud key file..."
+if [ ! -f $GCLOUD_KEY_FILE ]; then
+    echo "$GCLOUD_KEY_FILE not found!"
+    exit 1
+fi
+echo "#### Loading gcloud key file..."
+export GCLOUD_KEY=$(cat $GCLOUD_KEY_FILE)
+
 
 echo "#### Switching to docker daemon in Minikube"
 eval $(minikube docker-env)
