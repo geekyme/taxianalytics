01920e89a97866ec3e0a343c738193ee7bfdd873
Shawn Lim <shawnlim89@live.com>
2019-04-15 23:03:38 +0800

    Update k8s config to support multi replica

--- deploy.sh
+++ deploy.sh
@@ -2,28 +2,36 @@
 echo "#### Running script to deploy to Minikube..."
 
 APP=taxianalytics
-VERSION=$(git rev-parse --short HEAD)
 GCLOUD_KEY_FILE=key.json
 
+echo "#### Loading env variables..."
 if [ -z "$TAXI_TOPIC" ]
 then
   echo "env TAXI_TOPIC must be set!"
   exit 1
+else
+  echo "using env TAXI_TOPIC=$TAXI_TOPIC"
 fi
 
 if [ -z "$TAXI_PROJECT" ]
 then
   echo "env TAXI_PROJECT must be set!"
   exit 1
+else
+  echo "using env TAXI_PROJECT=$TAXI_PROJECT"
 fi
 
 if [ -z "$DB_HOST" ]
 then
   echo "env DB_HOST must be set!"
   exit 1
+else
+  echo "using env DB_HOST=$DB_HOST"
 fi
 
-echo "#### Current version: $VERSION"
+echo "#### Checking code version..."
+VERSION=$(git rev-parse --short HEAD)
+echo "Current version: $VERSION"
 export DOCKER_IMAGE_TAG=local/$APP:$VERSION
 
 echo "#### Finding gcloud key file..."
@@ -35,20 +43,21 @@ echo "#### Loading gcloud key file..."
 export GCLOUD_KEY=$(cat $GCLOUD_KEY_FILE)
 
 
-echo "#### Switching to docker daemon in Minikube"
+echo "#### Switching to docker daemon in Minikube..."
 eval $(minikube docker-env)
 
+echo "#### Building image..."
 docker build -t $DOCKER_IMAGE_TAG .
 
 echo "#### Updating deployment in Minikube..."
-
 cat kubernetes/$APP-deployment.yaml | ./envsubst | kubectl apply -f -
 
-kubectl expose deployment $APP --type=NodePort --name=$APP
+echo "#### Updating service in Minikube..."
+export EXTERNAL_IP=$(minikube ip)
+cat kubernetes/$APP-service.yaml | ./envsubst | kubectl apply -f -
 
+echo "#### Getting app url..."
 URL=$(minikube service $APP --url)
-
 echo "$APP deployed at $URL"
 
-echo "#### Switching to back docker daemon in host"
 eval $(minikube docker-env -u)
